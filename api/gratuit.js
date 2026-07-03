17733afe5001146adda55116d6fd1c59211f29b9
paultogbedji04 <paultogbedji04@gmail.com>
2026-07-03 11:13:23 +0000

    Fix api/gratuit.js : suppression champ nom inexistant dans table commandes

--- api/gratuit.js
+++ api/gratuit.js
@@ -27,7 +27,6 @@ module.exports = async (req, res) => {
       },
       body: JSON.stringify({
         email,
-        nom_client: nom || null,
         formation_id: formation_id || null,
         formation_titre,
         montant: '0€',
